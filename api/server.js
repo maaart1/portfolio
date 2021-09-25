6a47c87725522393941250b9a0e7f4f3772ec6ac
maaart1 <maaart1.thib@gmail.com>
2021-09-25 11:09:58 +0200

    Nav bar changed

--- api/server.js
+++ api/server.js
@@ -1,15 +1,19 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
+
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "/public/css"));
 
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
 app.get("/", (request, response) => {
-    response.render("./views/pages/index.ejs");
+    response.render("home");
 })
 
 app.get("/about", (request, response) => {
-    response.render("./views/pages/");
+    response.render("about");
 })
 
 app.get("*", (request, response) => {
